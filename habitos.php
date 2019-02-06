1d237437559155147f8f0d29d797164016e56a6f
Diego Rouco <diegoroucomarce3@outlook.es>
2019-02-06 09:37:16 +0100

    Ultima

--- habitos.php
+++ habitos.php
@@ -7,39 +7,59 @@
 <body>
 	<?php
 		include './database.php';
-	// Crear novo habito (resposta ao POST)
-		if (isset($_POST["nome"])) {
+		// Crear novo hábito (resposta ao POST)
+		if(isset($_POST["nome"])){
 			$insert = "INSERT INTO Habitos (Nome) VALUES ('" . $_POST["nome"] . "');";
 			$result = mysqli_query($conn, $insert);
 			echo $result;
-			echo "<p>Hábito " . $_POST["nome"] . ", creado</p>";
+			echo "<p>Hábito " . $_POST["nome"] . " creado</p>";
 		}
-	//Borrar hábito (resposta o get con parámetros)
+		// Borrar hábito (resposta ao GET con parámetros)
 		if (isset($_REQUEST["borrar"])) {
 			$delete = "DELETE FROM Habitos WHERE ID=" . $_REQUEST["borrar"] . ";";
 			$result = mysqli_query($conn, $delete);
 			echo $result;
+			echo "Hábito borrado";
 		}
 	?>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  <a class="navbar-brand" href="#">Habit Tracker</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarNav">
+		<ul class="navbar-nav">
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		  </li>
+		  <li class="nav-item active">
+		    <a class="nav-link" href="habitos.php">Hábitos</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="rexistro.php">Rexistro</a>
+		  </li>
+		</ul>
+	  </div>
+	</nav>
 	<h1>Hábitos</h1>
 	<?php
 		$lectura = "SELECT * FROM Habitos;";
 		$habitos = mysqli_query($conn, $lectura);
+		
 		if (mysqli_num_rows($habitos) > 0) {
-		echo "<ul class=\"list-group\">";
-		while($hab = mysqli_fetch_array($habitos)){
-		echo "<li class=\"list-group-item\">" . $hab['Nome'] . "<a href=\"habitos.php?borrar=" . $hab['ID'] . "\"><i class=\"far fa-trash-alt\"></i></a></li>";
-		}
-		echo "</ul>";
+			echo "<ul class=\"list-group\">";
+			while($hab = mysqli_fetch_array($habitos)){
+				echo "<li class=\"list-group-item\">" . $hab['Nome'] . " <a href=\"habitos.php?borrar=" . $hab['ID'] . "\">Borrar</a></li>";
+			}
+			echo "</ul>";
 		} else {
 			echo "Aínda non se creou ningún hábito";
 		}
-	
 	?>
-	<p>Se precisas axuda, lee <a href="https://www.youtube.com/">isto</a>.</p>
+	<p>Se precisas axuda, le <a href="https://habitualmente.com/pasos-para-cambiar-de-habitos/">isto</a>.</p>
 	<form name="habito" method="post" action="habitos.php">
 		<input type="text" id="nome" name="nome">
 		<button id="gardar" type="submit" class="btn btn-primary">Gardar</button>
-	</form>
+	</form>	
 </body>
 </html>
---
+++ rexistro.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+	<title>Rexistro</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+</head>
+<body>
+	<?php
+		include './database.php';
+
+		//Crera novo rexistro 
+		if ( isset($_REQUEST['crear']) ) {
+			$insertar= "INSERT INTO Rexistro ( id_habito, dia, valor) VALUES (" . $_REQUEST['crear'] . ",'" . $_REQUEST['data'] . "',1);";
+		$result =mysqli_query($conn, $insertar);
+		}		
+
+		$lectura = "SELECT * FROM Habitos ORDER BY Nome;";
+		$habitos = mysqli_query($conn, $lectura);
+		$lerexistro = "SELECT * FROM Rexistro INNER JOIN Habitos ON Rexistro.id_habito = Habitos.ID WHERE Rexistro.dia >= CURDATE() - INTERVAL 4 DAY ORDER BY Habitos.Nome, Rexistro.dia;";
+		$valores = mysqli_query($conn, $lerexistro);
+	?>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  <a class="navbar-brand" href="#">Habit Tracker</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarNav">
+		<ul class="navbar-nav">
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="habitos.php">Hábitos</a>
+		  </li>
+		  <li class="nav-item active">
+		    <a class="nav-link" href="#">Rexistro</a>
+		  </li>
+		</ul>
+	  </div>
+	</nav>
+	<table class="table">
+		<tr>
+			<td></td>
+			<?php
+				$hoxe = mktime(0,0,0);
+				$datas = [];
+				for ($dias=4;$dias>=0;$dias--) {
+					echo "<td>" . date('j/n/Y', $hoxe-$dias*24*60*60) . "</td>";
+					$datas[] = date('Y-m-d', $hoxe-$dias*24*60*60);
+				}
+			?>
+		</tr>
+		<?php
+			$valor = mysqli_fetch_array($valores);
+			while ($hab = mysqli_fetch_array($habitos)) {
+				echo "<tr><td>" . $hab['Nome'] . "</td>";
+				if ($valor['ID'] != $hab['ID']) {
+					foreach ($datas as $data) {
+						echo "<td><a href=\"rexistro.php?crear=" . $hab['ID'] . "&data=" . $data . "\"><button type<button type=\"button\" class=\"btn btn-light\"><i class=\"far fa-circle\"></i></button></a></td>";
+					}
+				} else {
+					foreach ($datas as $data) {
+						if (($valor['dia'] == $data) and ($valor['ID'] == $hab['ID'])) {
+							if ($valor['valor'] == 0) {
+								echo "<td><i class=\"fas fa-times-circle\"></i></td>";
+							} else {
+								echo "<td><i class=\"fas fa-check-circle\"></i></td>";
+							}
+							$valor = mysqli_fetch_array($valores);
+						} else {
+							echo "<td><a href=\"rexistro.php?crear=" . $hab['ID'] . "&data=" . $data . "\"><button type=\"button\" class=\"btn btn-light\"><i class=\"far fa-circle\"></i></button></a></td>";
+						}
+					}
+				}
+				echo "</tr>";
+			}
+		?>
+	</table>
+</body>
+</html>
